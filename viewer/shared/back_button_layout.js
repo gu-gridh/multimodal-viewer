ada69c33bcb186185f02f489802a27be58ab60c3
Tristan Bridge <tbridge360@gmail.com>
2026-07-08 10:37:06 -0700

    bug fix

--- viewer/shared/back_button_layout.js
+++ viewer/shared/back_button_layout.js
@@ -1,6 +1,7 @@
 try {
     document.documentElement.classList.toggle(
         'has-parent-back-button',
-        window.parent !== window && Boolean(window.parent.document.querySelector('.back-button, #back-button'))
+        window.parent !== window &&
+        Boolean(window.parent.document.querySelector('a[href] .back-button, a[href] #back-button'))
     );
 } catch {}
